682153b158ff9c2fd5599cc77ffdbf4ed9cbedea
Wellington Barros <wellington.moura.g@gmail.com>
2025-08-01 17:29:15 -0300

    Update streamlit_app.py
    connected changes needed bc of move from sis to snis

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,6 @@ st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
     """Choose the fruits you want in your custom Smoothie!
 """)
-st.write("""Choose the fruits you want in your custom Smoothie!""")
 
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
@@ -16,23 +15,13 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
-# Conexão com Snowflake
-conn = st.connection("snowflake")
 
-# Consulta os ingredientes disponíveis
-df = conn.query("SELECT FRUIT_NAME FROM smoothies.public.fruit_options", ttl="10m")
-
-# Lista de seleção
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
     , my_dataframe
     , max_selections=5
-    'Choose up to 5 ingredients:',
-    df["FRUIT_NAME"].tolist(),
-    max_selections=5
 )
 
-# Monta string de ingredientes
 if ingredients_list:
     ingredients_string = ''
     
@@ -46,16 +35,9 @@ my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_or
 
 #st.write(my_insert_stmt)
 #st.stop()
-    ingredients_string = ' '.join(ingredients_list)
 
-# Botão para enviar pedido
 time_to_insert = st.button('Submit Order')
 
 if time_to_insert:
     session.sql(my_insert_stmt).collect() 
-    insert_query = f"""
-        INSERT INTO smoothies.public.orders (ingredients, name_on_order)
-        VALUES ('{ingredients_string}', '{name_on_order}')
-    """
-    conn.query(insert_query, ttl=0)
     st.success('Seu Smoothie foi pedido!', icon="✅")
