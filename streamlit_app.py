430ca32f4596d246aec927b8e549f6980a743f49
Wellington Barros <wellington.moura.g@gmail.com>
2025-08-01 14:54:24 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,51 +1,40 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+from snowflake.snowpark.context import get_active_session
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
-st.write(
-    """Choose the fruits you want in your custom Smoothie!
-""")
+st.write("""Choose the fruits you want in your custom Smoothie!""")
 
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
-cnx = st.connection("snowflake")
-session = cnx.session()
-
+# Use active session from SIS app
 session = get_active_session()
+
+# Get fruit options
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
-    'Choose up to 5 ingredients:'
-    , my_dataframe
-    , max_selections=5
+    'Choose up to 5 ingredients:',
+    my_dataframe,
+    max_selections=5
 )
 
 if ingredients_list:
-    ingredients_string = ''
-    
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen +' '
+    ingredients_string = ' '.join(ingredients_list)
 
-#st.write(ingredients_string)
- 
-my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-        values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
-
-#st.write(my_insert_stmt)
-#st.stop()
+my_insert_stmt = f"""INSERT INTO smoothies.public.orders(ingredients, name_on_order)
+                     VALUES ('{ingredients_string}','{name_on_order}')"""
 
 time_to_insert = st.button('Submit Order')
 
 if time_to_insert:
-    session.sql(my_insert_stmt).collect() 
+    session.sql(my_insert_stmt).collect()
     st.success('Seu Smoothie foi pedido!', icon="✅")
 
 # new section to display smoothiefroot nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response)
-
